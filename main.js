2af236054f554ae33d64c7cb814c2d222bd4f231
Shpetim Haxhiu <shpetim.h@gmail.com>
2019-03-28 17:51:11 +0100

    Components

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-var app = new Vue({
-	el: '#app',
-	data: {
-		product: 'Socks',
-		brand: "Vue Mastery",
-		selectedVariant: 0,
-		details: ["80% cotton", "20% poyester", "Gender-neutral"],
-		variants: [
-		{
-			variantId: 2234,
-			variantImage: "images/vmSocks-green-onWhite.jpg",
-			variantColor: "green",
-			variantQuantity: 10
-		},
-		{
-			variantId: 2235,
-			variantImage: "images/vmSocks-blue-onWhite.jpg",
-			variantColor: "blue",
-			variantQuantity: 0
 
+Vue.component('product',
+	{
+	template: `
+		<div class="product">
+			<div class="product-image">
+				<img v-bind:src="image" alt="">
+			</div>
+			<div class="product-info">
+				<h1>{{ title }}</h1>
+				<p v-if="inStock">In Stock</p>
+				<p v-else>Out of Stock</p>
+
+				<ul>
+					<li v-for="detail in details">{{ detail }}</li>
+				</ul>
+
+				<div v-for="(variant, index) in variants" 
+					:key="variant.variantId"
+					class="color-box"
+					:style="{ backgroundColor: variant.variantColor }"
+					@mouseover="updateProduct(index)">
+
+				</div>
+
+				<button @click="addToCart"
+						:disable="!inStock"
+						:class="{ disabledButton: !inStock }">Add to cart</button>
+				
+				<div class="cart">
+					<p>Cart({{cart}})</p>
+				</div>
+			</div>
+		</div>
+	`,
+	data() {
+		return {
+			product: 'Socks',
+			brand: "Vue Mastery",
+			selectedVariant: 0,
+			details: ["80% cotton", "20% poyester", "Gender-neutral"],
+			variants: [
+				{
+					variantId: 2234,
+					variantImage: "images/vmSocks-green-onWhite.jpg",
+					variantColor: "green",
+					variantQuantity: 10
+				},
+				{
+					variantId: 2235,
+					variantImage: "images/vmSocks-blue-onWhite.jpg",
+					variantColor: "blue",
+					variantQuantity: 0
+
+				}
+			],
+			cart: 0
 		}
-		],
-		cart: 0
 	},
 
 	methods: {
@@ -49,5 +84,10 @@ var app = new Vue({
 		}
 	}
 
+	}
+);
 
-})
+
+var app = new Vue({
+	el: '#app'
+});
